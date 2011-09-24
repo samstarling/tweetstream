b08edd9d35eaa818796a50e2fbca92eca56de5a2
Sam Starling <sam.starling@gmail.com>
2011-09-24 16:04:06 +0100

    styles

--- web.js
+++ web.js
@@ -1,41 +1,30 @@
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
+var app = require('express').createServer(), io = require('socket.io').listen(app);
 
 io.set('transports', ['xhr-polling']); io.set('polling duration', 10);
 
 app.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/static/index.html');
+});
+
+app.get('/style.css', function (req, res) {
+  res.sendfile(__dirname + '/static/style.css');
+});
+
+var sys = require('sys');
+var twitter = require('twitter');
+var twit = new twitter({
+  consumer_key: 'rVGHP96wJj5pJUI1BqFSOg',
+  consumer_secret: 'It4so4h8UIkocgSbjoo2h2nYWy20vNM3KcHdXsxSo',
+  access_token_key: '19527505-MMnDFFdORLJRYSgvKEFmNvwdWZsKCaZD5DGmNPYsR',
+  access_token_secret: 'FEEczuM3ciakJPwLPl1vFJLiYJeC8V2w3FHwQxSC0E'
 });
 
 io.sockets.on('connection', function (socket) {
-  var sys = require('sys'),
-      twitter = require('twitter');
-  var twit = new twitter({
-      consumer_key: 'rVGHP96wJj5pJUI1BqFSOg',
-      consumer_secret: 'It4so4h8UIkocgSbjoo2h2nYWy20vNM3KcHdXsxSo',
-      access_token_key: '19527505-MMnDFFdORLJRYSgvKEFmNvwdWZsKCaZD5DGmNPYsR',
-      access_token_secret: 'FEEczuM3ciakJPwLPl1vFJLiYJeC8V2w3FHwQxSC0E'
-  });
-  twit.stream('user', {track:'London'}, function(stream) {
-      stream.on('data', function (data) {
-          //sys.puts(data.text);
-          socket.emit('tweet', data);
-      });
+  twit.stream('user', {track:'BBC'}, function(stream) {
+    stream.on('data', function (data) {
+      socket.emit('tweet', data);
+    });
   });
 });
-
-/*var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-*/
