fbecc74a86cf16af84622257acee1a74ac94143b
Sam Starling <sam.starling@gmail.com>
2011-10-07 21:41:32 +0100

    Prettying it up a bit

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
-var express = require('express'); 
+// Imports
+var express = require('express');
+var sys = require('sys');
+var twitter = require('twitter');
+
 var app = express.createServer();
-var io = require('socket.io').listen(app);
-
-io.set('transports', ['xhr-polling']); io.set('polling duration', 10);
-
 app.register('.html', require('jade'));
 app.set("view options", { layout: false });
 app.listen(process.env.PORT || 3000);
 
+var io = require('socket.io').listen(app);
+io.set('transports', ['xhr-polling']); io.set('polling duration', 10);
+
 app.get('/', function (req, res) {
   script_url = 'http://localhost';
   if(process.env.PORT) {
-      script_url = 'http://tweetstream.herokuapp.com';
+    script_url = 'http://tweetstream.herokuapp.com';
   }
   res.render(__dirname + '/public/index.html', {script_url: script_url});
 });
@@ -20,8 +23,6 @@ app.get('/style.css', function (req, res) {
   res.sendfile(__dirname + '/public/style.css');
 });
 
-var sys = require('sys');
-var twitter = require('twitter');
 var twit = new twitter({
   consumer_key: 'rVGHP96wJj5pJUI1BqFSOg',
   consumer_secret: 'It4so4h8UIkocgSbjoo2h2nYWy20vNM3KcHdXsxSo',
@@ -30,9 +31,8 @@ var twit = new twitter({
 });
 
 io.sockets.on('connection', function (socket) {
-  twit.stream('user', {track:'radio'}, function(stream) {
+  twit.stream('user', {track:'BBC'}, function(stream) {
     stream.on('data', function (data) {
-      sys.puts(data.text);
       socket.emit('tweet', data);
     });
   });
