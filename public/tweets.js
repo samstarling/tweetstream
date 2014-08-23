11d4c9bdd28c6f5300931f51e36c6ac2f171855e
Sam Starling <mail@samstarling.co.uk>
2014-08-23 17:35:36 +0100

    Added back parsing of URLs, hashtags and usernames

--- public/tweets.js
+++ public/tweets.js
@@ -8,10 +8,17 @@ $(document).ready(function() {
     $(".stats__pending .data").text(tweets.length);
   });
 
+  function parseTweet(text) {
+    return text
+      .replace(/(@\w+)/g, "<span class='username'>\$1</span>")
+      .replace(/(#\w+)/g, "<span class='hashtag'>\$1</span>")
+      .replace(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i, "<span class='url'>\$1</span>");
+  }
+
   setInterval(function() {
     var tweet = tweets.shift();
     if(tweet != null) {
-      $(".tweet__text").text(tweet.text);
+      $(".tweet__text").html(parseTweet(tweet.text));
       $(".tweet__user").text(tweet.user.screen_name);
       $(".tweet__time").text(tweet.created_at);
       $(".stats__pending .data").text(tweets.length);
