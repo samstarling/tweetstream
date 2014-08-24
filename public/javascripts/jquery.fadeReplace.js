993f0adbddec83cbf1c570da4ed7b244ae83be81
Sam Starling <mail@samstarling.co.uk>
2014-08-24 15:06:28 +0100

    Improved animation of username and time

--- public/javascripts/jquery.fadeReplace.js
+++ public/javascripts/jquery.fadeReplace.js
@@ -1,26 +1,30 @@
 (function($) {
-  $.fn.fadeReplace = function(text, formatter) {
-    console.log(formatter);
+  $.fn.fadeReplace = function(text, formatter, callback) {
     if($(this).children("span").length == 0) {
       $(this).html(textToSpans($(this).text(), formatter));
     }
-    replaceWords(this, textToSpans(text, formatter), 200, 20);
+    replaceWords(this, textToSpans(text, formatter), 200, 20, callback);
   };
 
-  function replaceWords(element, words, speed, delay) {
+  function replaceWords(element, words, speed, delay, callback) {
     $($(element).children("span").get().reverse()).each(function(i) {
       $(this).delay(delay * i).fadeOut(speed, function() {
         $(this).remove();
         if($(element).children("span").length == 0) {
-          setTimeout(function() { fadeInWords(element, words, speed, delay); }, delay);
+          setTimeout(function() { fadeInWords(element, words, speed, delay, callback); }, delay);
         }
       });
     });
   }
 
-  function fadeInWords(element, words, speed, delay) {
+  function fadeInWords(element, words, speed, delay, callback) {
+    callback();
     $(words.join("")).hide().appendTo(element).each(function(i) {
-      $(this).delay(delay * i).fadeIn(speed);
+      $(this).delay(delay * i).fadeIn(speed, function() {
+        if((i + 1) == $(element).children("span").length) {
+
+        }
+      });
     });
   }
 
--- public/javascripts/tweets.js
+++ public/javascripts/tweets.js
@@ -8,22 +8,25 @@ $(document).ready(function() {
     $(".stats__pending .data").text(tweets.length);
   });
 
-  function parseTweet(text) {
-    return
-  }
-
   setInterval(function() {
     var tweet = tweets.shift();
     if(tweet != null) {
-      console.log(tweet);
+
+      $(".tweet__user").fadeOut();
+      $(".tweet__time").fadeOut();
+
       $(".tweet__text").fadeReplace(tweet.text, function(text) {
         return text.replace(/(@\w+)/g, "<span class='username'>\$1</span>")
           .replace(/(#\w+)/g, "<span class='hashtag'>\$1</span>")
           .replace(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i, "<span class='url'>\$1</span>");
+      }, function() {
+        $(".tweet__user").text("@" + tweet.user.screen_name);
+        $(".tweet__time abbr").attr('title', tweet.created_at);
+        $(".timeago").timeago();
+        $(".tweet__user").fadeIn(500);
+        $(".tweet__time").fadeIn(500);
       });
-      $(".tweet__user").text("@" + tweet.user.screen_name);
-      $(".tweet__time abbr").attr('title', tweet.created_at);
-      $(".timeago").timeago();
+
       $(".stats__pending .data").text(tweets.length);
     }
   }, 6000)
